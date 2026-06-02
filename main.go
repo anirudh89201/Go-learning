069ed54e25d9b9131ff6df733b9fdaed05946b56
AnirudhReddy <44012589+anirudh89201@users.noreply.github.com>
2026-06-02 22:31:18 +0530

    02-06-2026
    Built a worker pool with 3 workers with WaitGroups and Go Routines

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/anirudh89201/hello/01-06-2026/interfaces"
+	"sync"
+	"time"
 )
 
-func main() {
-	store := interfaces.NewJSONStore("tasks.json")
-	manager := interfaces.NewManager(store)
-	_, err := manager.AddTask("hello World")
-	if err != nil {
-		fmt.Errorf("Failed to append to tasks.json %w", err)
+func Worker(idx int, jobs chan string, outputChannel chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		time.Sleep(3 * time.Second)
+		outputChannel <- fmt.Sprintf("Worker %d is processing %s", idx, job)
 	}
-	tasks, err := manager.GetTasks()
-	fmt.Println(tasks)
-	for _, val := range tasks {
-		fmt.Println(val.ID, val.Description)
+}
+func main() {
+	queue := []string{"A", "B", "C", "D", "E", "F"}
+	jobs := make(chan string, len(queue))
+	outputChannel := make(chan string, len(queue))
+	var wg sync.WaitGroup
+	for i := range 3 {
+		wg.Add(1)
+		go Worker(i, jobs, outputChannel, &wg)
+	}
+	for _, job := range queue {
+		jobs <- job
+	}
+	close(jobs)
+	go func() {
+		wg.Wait()
+		close(outputChannel)
+	}()
+	for val := range outputChannel {
+		fmt.Println(val)
 	}
 }
